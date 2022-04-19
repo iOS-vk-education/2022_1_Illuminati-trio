cc0577256cb162cd3ae54423495f1b0b26f1939c
Flatty11 <kosov11@gmail.com>
2022-04-20 00:18:29 +0300

    A lot of changes Пока сырой функционал: - экран теории берет названия из сети и по ним загружает информацию - экран задач берет задачи из сети, по их подпунктам ищет задачи и их условие(div элементы страницы) Практически все сделано через распарсивание html страниц, поиск текста и условие задач код еще можно улучшать, архитектуры пока ярко выраженной пока нет

--- EGEKit/ExerciseViewController.swift
+++ EGEKit/ExerciseViewController.swift
@@ -7,13 +7,126 @@
 
 import Foundation
 import UIKit
+import PinLayout
+import HTMLKit
+
+class Section {
+    var title: String
+    let options: [String]
+    var isOpened: Bool = false
+    
+    init(title: String,options: [String],isOpened: Bool = false) {
+        self.title = title
+        self.options = options
+        self.isOpened = isOpened
+    }
+}
 
 class ExerciseViewController: UIViewController {
     
+    private let titleOfScreen = UILabel()
+    private let titleInfo = UILabel()
+    private var tableView = UITableView()
+
+    private var sections = [Section]()
+    private var subExUrls = [[String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        let sectionNames = NetworkManager.shared.getSectionsName()
+        
+        let subExNames = NetworkManager.shared.getSubExNames()
+        subExUrls = NetworkManager.shared.getSubExUrls()
+        
+        for i in 0..<sectionNames.count {
+            sections.append(Section(title: sectionNames[i], options: subExNames[i] ) )
+        }
+        
+        
         view.backgroundColor = .systemBackground
+        
+        titleOfScreen.text = "Каталог заданий"
+        titleOfScreen.font = .boldSystemFont(ofSize: 20)
+        
+        titleInfo.text = "Здесь представлен список задач, разделенный по типам"
+        titleInfo.textColor = .systemGray
+        titleInfo.numberOfLines = 2
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(.init(nibName: "LoreTableViewCell", bundle: nil), forCellReuseIdentifier: "LoreTableViewCell")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        view.addSubview(tableView)
+        view.addSubview(titleOfScreen)
+        view.addSubview(titleInfo)
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        titleOfScreen.pin
+            .topCenter(60)
+            .sizeToFit()
+        
+        titleInfo.pin.below(of: titleOfScreen)
+            .left().right().marginHorizontal(10).height(60)
+        
+        tableView.pin.below(of: titleInfo).left().right().bottom()
+    }
+    
+}
+
+extension ExerciseViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        sections.count
+    }
+            
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        
+        if section.isOpened {
+            return section.options.count + 1
+        }
+        else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if indexPath.row == 0 {
+            let font = UIFont.systemFont(ofSize: 16, weight: .bold)
+            cell.textLabel?.font = font
+            cell.textLabel?.text = sections[indexPath.section].title
+            return cell
+        }
+        else {
+            let font = UIFont.systemFont(ofSize: 15, weight: .light)
+            cell.textLabel?.font = font
+            cell.textLabel?.text = sections[indexPath.section].options[indexPath.row - 1]
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section \(indexPath.section) Row: \(indexPath.row )")
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        } else {
+            let viewC = ExerciseDetViewController(urlString: subExUrls[indexPath.section][indexPath.row - 1],
+                                                  title0: sections[indexPath.section].options[indexPath.row - 1])
+            let navC = UINavigationController(rootViewController: viewC)
+            present(navC, animated: true, completion: nil)
+        }
+    }
+    
     
 }
--- EGEKit/TheoryViewController.swift
+++ EGEKit/TheoryViewController.swift
@@ -7,12 +7,88 @@
 
 import Foundation
 import UIKit
+import PinLayout
 
 class TheoryViewController: UIViewController {
     
+    private let titleOfScreen = UILabel()
+    private let titleInfo = UILabel()
+    private var tableView = UITableView()
+    let urlTheory = "https://math-ege.sdamgia.ru/page/theory"
+    var theoryAll:[Theory] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        theoryAll = NetworkManager.shared.loadTheory(with: urlTheory)
+        
+        titleOfScreen.text = "Полезный материал"
+        titleOfScreen.font = .boldSystemFont(ofSize: 20)
+        
+        titleInfo.text = "Теоретический материал, разделенный по типам задач, который необходим для их решения"
+        titleInfo.textColor = .systemGray
+        titleInfo.numberOfLines = 2
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        view.addSubview(tableView)
+        view.addSubview(titleOfScreen)
+        view.addSubview(titleInfo)
+        
+        tableView.tableFooterView = UIView()
+        
         view.backgroundColor = .systemBackground
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        titleOfScreen.pin
+            .topCenter(60)
+            .sizeToFit()
+        
+        titleInfo.pin.below(of: titleOfScreen)
+            .left().right().marginHorizontal(10).height(60)
+        
+        tableView.pin.below(of: titleInfo).left().right().bottom()
+    }
+    
+    func open(with urlString: String, title: String) {
+        
+        let viewC = TheoryDetailsViewController(urlString: urlString,title0: title)
+        let navC = UINavigationController(rootViewController: viewC)
+        present(navC, animated: true, completion: nil)
+        
+    }
+}
+
+extension TheoryViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.open(with: theoryAll[indexPath.row].urlString, title: theoryAll[indexPath.row].name)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        theoryAll.count
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+            
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var defaultConf = cell.defaultContentConfiguration()
+        defaultConf.text = theoryAll[indexPath.row].name
+        cell.contentConfiguration = defaultConf
+        
+        return cell
+    }
+
 }
